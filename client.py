eded4c6d5df1dce202e519e9b993888e92d5db98
Masashi Takemoto <lesser@beatcraft.jp>
2020-01-23 21:17:28 +0900

    fix

--- client.py
+++ client.py
@@ -20,29 +20,29 @@ import netutil
 #
 #
 class ClientLooper(netutil.Looper):
-    def __init__(self, local_addr, local_port, remote_addr, remote_port):
+    def __init__(self, local_addr, local_port, remote_addr, remote_port, batch_size, batch_start, device_id, package_id):
         print "ClientLooper::__init__()"
         super(ClientLooper, self).__init__(local_addr, local_port, remote_addr, remote_port)
+        #
+        self._batch_size = batch_size
+        self._batch_start = batch_start
+        self._device_id = device_id
+        self._package_id = package_id
         #
         self._send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
     def setup(self):
-        package_id = 0 # MNIST
-        # 0 : AMD Server"
-        # 1 : Intel on MBP"
-        # 2 : eGPU (AMD Radeon Pro 580)"
         platform_id = 0
-        device_id = 0
-        my_gpu = gpu.Gpu(platform_id, device_id)
+        my_gpu = gpu.Gpu(platform_id, self._device_id)
         my_gpu.set_kernel_code()
-        self._package = util.Package(package_id)
+        self._package = util.Package(self._package_id)
         self._roster = self._package.setup_dnn(my_gpu)
         if self._roster is None:
             print "fatal DNN error"
         #
         # set batch
-        batch_start = 0
-        batch_size = 10000
+        batch_start = self._batch_start
+        batch_size = self._batch_size
         self._package.load_batch()
         self._roster.set_batch(self._package._train_image_batch, self._package._train_label_batch, batch_start, batch_size, self._package._image_size, self._package._num_class, 0)
         
@@ -50,7 +50,6 @@ class ClientLooper(netutil.Looper):
         self._roster.propagate()
         ce = self._roster.get_cross_entropy()
         print ce
-        
         
     def loop(self):
         print "ClientLooper::loop() - start"
@@ -67,7 +66,6 @@ class ClientLooper(netutil.Looper):
             # decode and response
             #
             a, b, c, d, e = netutil.unpack_i5(res)
-            #print a
             if a==10: # init
                 print "init"
                 cmd = netutil.pack_if(15, 1.0)
@@ -80,7 +78,6 @@ class ClientLooper(netutil.Looper):
             elif a==30: # alt
                 self._roster.propagate(b, c, d, e, 0)
                 ce = self._roster.get_cross_entropy()
-                #print "set_alt(%d, %d, %d, %d)=%f" %(b, c, d, e, ce)
                 cmd = netutil.pack_if(35, ce)
                 self.send(cmd)
             elif a==40: # update
@@ -89,25 +86,21 @@ class ClientLooper(netutil.Looper):
                 layer.update_weight_gpu()
                 self._roster.propagate()
                 ce = self._roster.get_cross_entropy()
-                #print "update(%d, %d, %d, %d)=%f" %(b, c, d, e, ce)
-                #print ce
                 cmd = netutil.pack_if(45, ce)
                 self.send(cmd)
             else:
                 print "unknown command"
                 pass
+            # if
+        # while
+    # end of loop()
 #
 #
 #
-def main():
-    print "main() : start"
+def client(BC_ADDR, BC_PORT, SERVER_ADDR, SERVER_PORT, batch_size, batch_start, device_id, package_id):
+    print "client()"
     #
-    BC_ADDR = "192.168.200.255"
-    BC_PORT = 5000
-    SERVER_ADDR = "192.168.200.10"
-    SERVER_PORT = 5005
-    #
-    c = ClientLooper(BC_ADDR, BC_PORT, SERVER_ADDR, SERVER_PORT)
+    c = ClientLooper(BC_ADDR, BC_PORT, SERVER_ADDR, SERVER_PORT, batch_size, batch_start, device_id, package_id)
     c.init()
     c.run()
     #
@@ -120,6 +113,26 @@ def main():
             c.quit()
     #
     print "main() : end"
+#
+#
+#
+def main():
+    print "main() : start"
+    #
+    BC_ADDR = "192.168.200.255"
+    BC_PORT = 5000
+    SERVER_ADDR = "192.168.200.10"
+    SERVER_PORT = 5005
+    #
+    batch_size = 5000
+    batch_start = 0
+    device_id = 0
+    # 0 : AMD Server"
+    # 1 : Intel on MBP"
+    # 2 : eGPU (AMD Radeon Pro 580)"
+    package_id = 0 # MNIST
+    #
+    client(BC_ADDR, BC_PORT, SERVER_ADDR, SERVER_PORT, batch_size, batch_start, device_id, package_id)
 #
 #
 #
---
+++ client_0-0.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import argparse
+import traceback
+import csv
+import socket
+import time
+import multiprocessing
+import struct
+import binascii
+#
+import numpy as np
+import sys,os
+import core
+import util
+import gpu
+import netutil
+import client
+#
+#
+#
+def main():
+    print "main() : start"
+    #
+    BC_ADDR = "192.168.200.255"
+    BC_PORT = 5000
+    SERVER_ADDR = "192.168.200.10"
+    SERVER_PORT = 5005
+    # 0 : 192.168.0.150 / 192.168.200.10
+    batch_size = 5000
+    batch_start = 0
+    device_id = 0
+    # 1-0 : 192.168.0.150 / 192.168.200.11
+#   batch_size = 5000
+#   batch_start = 5000
+#   device_id = 0
+    # 1-1 : 192.168.0.151 / 192.168.200.11
+#   batch_size = 5000
+#   batch_start = 10000
+#   device_id = 1
+    # 2-0 : 192.168.0.152 / 192.168.200.12
+#   batch_size = 10000
+#   batch_start = 15000
+#   device_id = 0
+    # 0 : AMD Server"
+    # 1 : Intel on MBP"
+    # 2 : eGPU (AMD Radeon Pro 580)"
+    package_id = 0 # MNIST
+    #
+    client.client(BC_ADDR, BC_PORT, SERVER_ADDR, SERVER_PORT, batch_size, batch_start, device_id, package_id)
+#
+#
+#
+if __name__ == '__main__':
+	main()
+
+

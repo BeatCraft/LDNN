bd70ce95573530f628518e57f61856e47a898e05
Masashi Takemoto <lesser@beatcraft.jp>
2020-03-12 17:50:49 +0900

    fix

--- gpu.py
+++ gpu.py
@@ -4,6 +4,9 @@ from time import time
 import numpy as np
 #
 #
+# WEIGHT_SET_0 = [-1.0, -0.5, -0.25, -0.125, -0.0625, -0.03125, -0.015625, -0.0078125,
+#                  0.0,
+#                  0.0078125, 0.015625, 0.03125, 0.0625, 0.125, 0.25, 0.5, 1.0]
 #
 KERNEL_CODE = """
 __kernel void scale(

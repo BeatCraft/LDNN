36ef50aa038e393a981043bb2f1f93e425630660
Masashi Takemoto <lesser@beatcraft.jp>
2021-02-02 21:05:39 +0900

    Update mpi.sh

--- mpi.sh
+++ mpi.sh
@@ -6,7 +6,6 @@ if [ $# == 0 ] ; then
     echo "no number pf process"
     echo "ie. mpi.sh 2"
 elif [ $# == 1 ] ; then
-    ls $1
     mpiexec -host localhost:$1 -n $1 python ./mpi_test.py
 else
     echo "too many options"

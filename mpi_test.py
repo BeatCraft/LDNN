bde8315fab1473f7a966d3ae22d6c99be0db448b
Masashi Takemoto <lesser@beatcraft.jp>
2021-05-26 14:56:12 +0900

    Update mpi_test.py

--- mpi_test.py
+++ mpi_test.py
@@ -94,7 +94,7 @@ def main():
     #
     wk = worker(com, package_id, config_id)
 #    wk._train.mpi_loop(com, rank, size)
-    wk._train.mpi_cnn_loop(self, 20, 1, com, rank, size)
+    wk._train.mpi_cnn_loop(20, 1, com, rank, size)
     return 0
 #
 #

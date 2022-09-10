16331f995653f10152da811f188c4580f5eb1bbf
Masashi Takemoto <lesser@beatcraft.com>
2022-09-10 13:16:35 +0900

    up

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 LesserDNN : a DNN with Discrete Weights
 
+
+Following python modules are required:
+- numpy
+- PIL
+- pyopencl
+- cupyx
+- mpi4py
+
+
 How to install
+clone the repository or download the zip file, and unzip it.
+
+
+Quick start
+python3 ./core.py
+No error if eberything is OK:)
+
+
+
+
+
--- core.py
+++ core.py
@@ -1187,12 +1187,36 @@ class Roster:
 #
 #
 def main():
-    r = Roster()
+    import plat
+    if plat.ID==0: # MBP
+        import opencl
+        platform_id = 0
+        device_id = 1
+        my_gpu = opencl.OpenCL(platform_id, device_id)
+        my_gpu.set_kernel_code()
+    else:
+        return 0
     #
-    input_layer = r.add_layer(0, 196, 196)
-    hidden_layer_1 = r.add_layer(1, 196, 32)
-    hidden_layer_2 = r.add_layer(1, 32, 32)
-    output_layer = r.add_layer(2, 32, 10)
+
+    r = Roster()
+    r.set_gpu(my_gpu)
+    #
+    c = r.count_layers()
+    input = InputLayer(c, 128, 128, None, my_gpu)
+    r.layers.append(input)
+    
+    c = r.count_layers()
+    hidden_1 = HiddenLayer(c, 128, 64, input, my_gpu)
+    r.layers.append(hidden_1)
+    
+    c = r.count_layers()
+    hidden_2 = HiddenLayer(c, 64, 64, hidden_1, my_gpu)
+    r.layers.append(hidden_2)
+
+    c = r.count_layers()
+    output = OutputLayer(c, 64, 10, hidden_2, my_gpu)
+    r.layers.append(output)
+
     r.init_weight()
     #
     return 0
